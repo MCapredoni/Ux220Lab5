3531a1cf8c59aafa3b22675f88ce6ba81459b4c1
MCapredoni <capr5033@mylaurier.ca>
2023-11-08 20:51:47 -0500

    updated files

--- customelement.js
+++ customelement.js
@@ -4,4 +4,21 @@ class MarcElement extends HTMLElement{
     }
 }
 
-customElements.define("x-marc/, MarcElement");
+customElements.define("x-marc", MarcElement);
+
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = 
+        `<div style="text-align:center">${this.innerHTML}</div>`;
+    }
+}
+
+customElements.define("x-center", CenterElement);
+
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new date().getFullYear();
+    }
+}
+
+customElements.define("x-fullyear", YearElement);
